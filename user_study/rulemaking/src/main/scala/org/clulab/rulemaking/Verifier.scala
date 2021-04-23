de09e61156b5f0070044b977b03f2d143ee49aab
bsharpataz <bsharp@email.arizona.edu>
2021-04-23 13:36:58 -0700

    added scala proj

---
+++ user_study/rulemaking/src/main/scala/org/clulab/rulemaking/Verifier.scala
@@ -0,0 +1,12 @@
+package org.clulab.rulemaking
+
+class Verifier {
+  // verify that a pattern matches
+
+  // does the pattern match other things?
+
+  // load the full_graph --> convert to Document
+
+  // index the document for the EE
+
+}
